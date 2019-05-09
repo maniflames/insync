5dee27fa8518db8b7445cd0923217e0d34335383
Imani Dap <imani.dap@gmail.com>
2019-05-10 00:53:55 +0200

    chore(collision): add base functionality of collision system

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use three;
 use three::Object;
 use recs::{Ecs, EntityId, component_filter};
 use rand::Rng;
+use mint::Point3;
 
 #[derive(Clone, PartialEq, Debug, Copy)]
 struct Position {
@@ -13,7 +14,76 @@ struct Position {
 #[derive(Clone, PartialEq, Debug)]
 struct GameObject {
     mesh: three::Mesh,
-    is_player: bool
+    is_player: bool,
+    vertices: Vec<Point3<f32>>
+}
+
+
+fn collision_system(store: &mut recs::Ecs) {
+    //NOTE: usage of skeletons would be nice since meshes can be used and the game can move away from basic shapes
+    //NOTE: in the next iteration dimentions should at least be precalculated from the vertices in the base shape. 
+
+    let component_filter = component_filter!(Position, GameObject);
+    let mut entities: Vec<EntityId> = Vec::new(); 
+    // (EntityId, minX, maxX, minY, maxY, minZ, maxZ)
+    let mut enemies: Vec<(&EntityId, f32, f32, f32, f32, f32, f32)> = Vec::new(); 
+    let mut player = (0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+    
+    store.collect_with(&component_filter, &mut entities);
+        let find_min = |min, current| {
+            if current < min {
+                return current;
+            }
+
+            return min;
+        };
+
+        let find_max = |max, current| {
+            if current > max {
+                return current;
+            }
+
+            return max;
+        };
+
+    for entity in entities.iter() {
+        let gameobject = store.get::<GameObject>(*entity).unwrap(); 
+        let position = store.get::<Position>(*entity).unwrap(); 
+  
+        let x_values = gameobject.vertices.iter().map(|vertex| vertex.x );
+        let x_min = x_values.clone().fold(0.0, find_min); 
+        let x_max = x_values.clone().fold(0.0, find_max);
+
+        let y_values = gameobject.vertices.iter().map(|vertex| vertex.y );
+        let y_min = y_values.clone().fold(0.0, find_min); 
+        let y_max = y_values.clone().fold(0.0, find_max);
+
+        let z_values = gameobject.vertices.iter().map(|vertex| vertex.z );
+        let z_min = z_values.clone().fold(0.0, find_min); 
+        let z_max = z_values.clone().fold(0.0, find_max);
+
+        if !gameobject.is_player {
+            enemies.push((entity, x_min + position.x, x_max + position.x, y_min + position.y, y_max + position.y, z_min + position.z, z_max + position.z));
+        }
+
+        if gameobject.is_player {
+            player = (x_min + position.x, x_max + position.x, y_min + position.y, y_max + position.y, z_min + position.z, z_max + position.z);
+        }
+    }
+
+    for enemy in enemies {
+        let (player_x_min, player_x_max, player_y_min, player_y_max, player_z_min, player_z_max) = player;
+        let (entity, enemy_x_min, enemy_x_max, enemy_y_min, enemy_y_max, enemy_z_min, enemy_z_max) = enemy;
+
+        if player_x_min < enemy_x_max && player_x_max > enemy_x_min {
+            if player_y_min < enemy_y_max && player_y_max > enemy_y_min {
+                if player_z_min < enemy_z_max && player_z_max > enemy_z_min {
+                    println!("hit!");
+                    //TODO: remove entity & mesh from world
+                }
+            }
+        }
+    }
 }
 
 fn positioning_system(window: &mut three::Window, store: &mut recs::Ecs) {
@@ -77,9 +147,11 @@ fn meteor_factory(window: &mut three::Window, store: &mut Ecs, num_meteors: i32)
             .. Default::default()
         };
 
+        let vertices = geometry.base.vertices.clone();
+
         let mesh = window.factory.mesh(geometry, material); 
         window.scene.add(&mesh);
-        let _ = store.set(cube, GameObject{mesh: mesh, is_player: false});
+        let _ = store.set(cube, GameObject{mesh: mesh, is_player: false, vertices: vertices});
     }
 }
 
@@ -93,9 +165,12 @@ fn player_factory(window: &mut three::Window, store: &mut Ecs) {
         .. Default::default()
     };
 
+    let vertices = geometry.base.vertices.clone();
+
     let mesh = window.factory.mesh(geometry, material); 
     window.scene.add(&mesh);
-    let _ = store.set(player, GameObject{mesh: mesh, is_player: true});
+
+    let _ = store.set(player, GameObject{mesh: mesh, is_player: true, vertices: vertices});
 }
 
 fn main() {
@@ -109,8 +184,9 @@ fn main() {
     let mut store = Ecs::new();
     meteor_factory(&mut window, &mut store, 2);
     player_factory(&mut window, &mut store);
-
+    
     while window.update() {
+        collision_system(&mut store); 
         positioning_system(&mut window, &mut store);
         window.render(&camera);
     }
